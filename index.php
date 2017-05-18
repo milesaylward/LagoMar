9a810f1c89107d5fbcf3e440562416e26c1eeac5
Miles Aylward <miles.aylward@gmail.com>
2017-05-18 09:55:37 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lago Mar Pizza</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <div class="img-header">
+      <div class="header-wrap">
+        <h1 class="welcome">Welcome to</h1>
+        <img src="images/white-logo.png" alt="Lago Mar Pizza" class="header-logo img-responsive">
+        <h1 class="welcome options text-center">Dine In | Take-Out | Delivery</h1>
+      </div>
+      <a href="#main" class="scroll-main"><i id="scroll" class="fa fa-angle-down welcome text-center" aria-hidden="true"></i></a>
+    </div>
+    <div class="main" id="main">
+      <?php include("includes/nav.php") ?>
+    </div>
+  </body>
+  <script src="js/index.js" charset="utf-8"></script>
+</html>
---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(function(){
+  $('body').scrollspy({
+    target: ".scroll-main", offset: $(window).height() * 1
+  });
+
+  $('.scroll-main').on('click', function(event) {
+
+    var hash = this.hash;
+    console.log(hash);
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000, () => {
+      window.location.hash = hash;
+      });
+    }
+  );
+
+  let $foodMenu = $('.food-menu');
+  let $home = $('.home');
+
+  $foodMenu.on('click', () =>{
+    $home.removeClass('active');
+    $foodMenu.addClass('active');
+  });
+  $('body').on('click', event =>{
+    if(event.target.id != 'food-menu-toggle'){
+      $('.food-menu').removeClass('active');
+      $(".home").addClass('active');
+    }
+  });
+});
